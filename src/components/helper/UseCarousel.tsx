06b5077cea30bc54d36343dcb075e73b993236d1
Hannah Carabolante <carabolanteh@gmail.com>
2022-05-16 10:26:20 -0500

    Carousel costum

--- src/components/helper/UseCarousel.tsx
+++ src/components/helper/UseCarousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import  "../../scss/Main.scss";
 
 const UseCarousel = ({datos}) => {
   
@@ -10,11 +11,11 @@ const UseCarousel = ({datos}) => {
       infiniteLoop
       showArrows={false}
       centerMode
-      dynamicHeight
+      // dynamicHeight
     >
       {datos?.map(data=>
         <div key={data.title}>
-            <img src={data.src} alt={data.title} style={{ height: '5%',  width: '50%' }} />
+            <img src={data.src} alt={data.title} style={{  width: '30%' }} />
         </div>
       )}
         
