668dacb741a90a1d5ac003224d37b7ed7d8a6ebf
Matias Benary <matiasbenary@gmail.com>
2022-06-08 22:57:25 -0300

    merge

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,7 +4,6 @@ import { Box } from "@mui/material";
 
 function Presentation() {
   return (
-<<<<<<< HEAD
     // <div >
     <Box gap={2} display="flex" mt={"8%"} className="container">
       <div sx={12} md={8} lg={10} xl={10}>
@@ -28,25 +27,6 @@ function Presentation() {
     </Box>
     // </div>
   );
-=======
-        <Box display="flex" mt={'8%'} className='container'>
-          <Box  className='box-title-p'>
-            <h1 className='title'>
-              Soluciones para facilitar el impacto social de las empresas
-            </h1>
-            <p className='title_p paragraph'>
-              Fonselp es la plataforma ideal para donar, realizar compras responsables, compartir el conocimiento de tus equipos, o conectar con organizaciones que están cambiando el mundo.
-            </p>
-            <button className='btn-registrate'>
-              Registrate gratis
-            </button>
-          </Box>
-          <Box className='box-img-presentation'>
-            <img src="../assets/image-hero.png" alt=" Ilustracion para soluciones que facilitan el impacto social de las empresas" className='box-img-presentation_img-hero'/>
-          </Box>
-        </Box>
-  )
->>>>>>> afb7ca61b17057831574d7471269b9660cf78642
 }
 
 export default Presentation;
